09a3d8c66946a7e358a17923b9487f99fcbd740e
Arthur Annibal <arthur.annibal@gmail.com>
2024-04-24 03:01:29 -0300

    nbtpi update

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ function nbtpi() {
         // if not exact html match
         if (nbtpiElm.outerHTML !== nbtpiStr) {
           console.log("Invalid NBTPI 4");
-          console.log({ a: nbtpiElm.outerHTML, b: nbtpiStr })
+          console.log({ a: nbtpiElm.outerHTML, b: nbtpiStr });
           return injectNBTPI();
         }
 
@@ -81,7 +81,7 @@ function nbtpi() {
 
 setTimeout(() => {
   nbtpi();
-}, 5000)
+}, 5000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
